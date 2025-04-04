0481f4b5c900fc22dc94716e46593dc819cd19ec
whoami-saloni <148846161+whoami-saloni@users.noreply.github.com>
2025-04-04 19:59:52 +0530

    Prepare Base Model Completed

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "src")))
 from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_02_Prepare_base_model import PrepareBaseModelTrainingPipeline
 
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
 STAGE_NAME= "Data Ingestion Stage"
+STAGE_NAME1= "Preparing Base Model"
 
 try:
     logger.info(f">>>>>>>stage {STAGE_NAME} started <<<<<<<<")
@@ -16,3 +18,13 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+try:
+    logger.info(f">>>>>>>stage {STAGE_NAME1} started <<<<<<<<")
+    obj=PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>>> stage {STAGE_NAME1} completed<<<<<\n\nx=========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,6 +1,6 @@
 from cnnClassifier.constants import * 
 from cnnClassifier.utils.common import read_yaml,create_directory
-from cnnClassifier.entity.config_entity import DataIngestionConfig
+from cnnClassifier.entity.config_entity import DataIngestionConfig,PrepareBaseModelConfig
 class ConfigurationManager:
     def __init__(
         self,
@@ -24,3 +24,22 @@ class ConfigurationManager:
             unzip_dir=config.unzip_dir
         )
         return data_ingestion_config
+    
+    def get_prepare_base_model_config(self) ->PrepareBaseModelConfig:
+        config =self.config.prepare_base_model
+
+        create_directory([config.root_dir])
+        prepareBaseModelConfig=PrepareBaseModelConfig(
+            root_dir =Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            params_image_size=self.params.IMAGE_SIZE,
+            params_learning_rate=self.params.LEARNING_RATE,
+            params_include_top=self.params.INCLUDE_TOP,
+            params_weights=self.params.WEIGHTS,
+            params_classes=self.params.CLASSES
+        )
+
+        return prepareBaseModelConfig
+
+        
